0e39e28f4db89087c25bcecca288aadcbaacc51d
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-09-04 12:45:40 -0300

    wip

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 class Face {
-    /** @param {number[]} vertices */
-    constructor(vertices) {
-        this.vertices = vertices;
+    /** @param {number[]} verticesIndices */
+    constructor(verticesIndices) {
+        this.vertices = verticesIndices;
     }
 }
 
@@ -11,6 +11,7 @@ class Model {
      * @param {Face[]} faces 
      */
     constructor(vertices, faces){
+        this.vertices = vertices;
         this.faces = faces;
     }
 }
@@ -20,6 +21,22 @@ class Object {
     constructor(model){
         this.model = model;
     }
+}
+
+function multiplyMatrices(m1, m2) {
+    const result = [];
+    for (let i = 0; i < m1.length; i++) {
+        result[i] = [];
+
+        for (let j = 0; j < m2[0].length; j++) {
+            let sum = 0;
+            for (var k = 0; k < m1[0].length; k++)
+                sum += m1[i][k] * m2[k][j];
+
+            result[i][j] = sum;
+        }
+    }
+    return result;
 }
 
 const cuboVertices = [
@@ -61,40 +78,40 @@ const cuboVertices = [
 ];
 const cuboModel = new Model(cuboVertices, [
     new Face([ //face traseira
-        cuboVertices[0],
-        cuboVertices[1],
-        cuboVertices[2],
-        cuboVertices[3],
+        0,
+        1,
+        2,
+        3,
     ]),
     new Face([ //face frontal
-        cuboVertices[4],
-        cuboVertices[5],
-        cuboVertices[6],
-        cuboVertices[7],
+        4,
+        5,
+        6,
+        7,
     ]),
     new Face([ //face superior
-        cuboVertices[0],
-        cuboVertices[1],
-        cuboVertices[5],
-        cuboVertices[4],
+        0,
+        1,
+        5,
+        4,
     ]),
     new Face([ //face inferior
-        cuboVertices[3],
-        cuboVertices[2],
-        cuboVertices[6],
-        cuboVertices[7],
+        3,
+        2,
+        6,
+        7,
     ]),
     new Face([ //face esquerda
-        cuboVertices[0],
-        cuboVertices[4],
-        cuboVertices[7],
-        cuboVertices[3],
+        0,
+        4,
+        7,
+        3,
     ]),
     new Face([ //face direita
-        cuboVertices[1],
-        cuboVertices[5],
-        cuboVertices[6],
-        cuboVertices[2],
+        1,
+        5,
+        6,
+        2,
     ]),
 ]);
 
@@ -115,13 +132,73 @@ const ctx = canvas.getContext("2d");
 // desenharQuadrado();
 
 /** @param {Model} model */
-function drawModel(model, scale = 1, offset_x = 0, offset_y = 0){
-    for(let face of model.faces){
+function renderModel(model, scale = 1, offset_x = 0, offset_y = 0){
+    /** @type {Model} */
+    const copy = JSON.parse(JSON.stringify(model));
+
+    for(let vertice of copy.vertices)
+        vertice.push(1); //adiciona a coordenada homogênea
+
+    let projetado = multiplyMatrices(copy.vertices, [
+        [ 0.707,  0.408, 0, 0 ],
+        [ 0,      0.816, 0, 0 ],
+        [ 0.707, -0.408, 0, 0 ],
+        [ 0,      0,     0, 1 ],
+    ]);
+
+    for(let i = 0; i < projetado.length; i++)
+    for(let j = 0; j < projetado[i].length; j++)
+        copy.vertices[i][j] = projetado[i][j];
+
+    //TODO: tentar fazer um algorítimo para ordenar as faces antes de renderizar (da mais longe para a mais perto)
+    // copy.faces.sort((a, b) => {
+    //     const verticesA = a.vertices.map(i => copy.vertices[i]);
+    //     const verticesB = b.vertices.map(i => copy.vertices[i]);
+
+    //     const avgA = verticesA.reduce((acc, v) => [
+    //         acc[0] + v[0],
+    //         acc[1] + v[1],
+    //         acc[2] + v[2]
+    //     ], [0, 0, 0]).map(x => x / verticesA.length);
+
+    //     const avgB = verticesB.reduce((acc, v) => [
+    //         acc[0] + v[0],
+    //         acc[1] + v[1],
+    //         acc[2] + v[2]
+    //     ], [0, 0, 0]).map(x => x / verticesB.length);
+
+    //     console.log({ avgA, avgB });
+
+    //     return Math.sign(avgB[1] - avgA[1]);
+    // });
+
+    const colors = [
+        'red',
+        'yellow',
+        'blue',
+        'magenta',
+        'green',
+        'black'
+    ];
+
+    let colorIndex = 0;
+
+    for(let face of copy.faces){
         ctx.beginPath();
-        for(let vertex of face.vertices)
+
+        for(let n = 0; n < face.vertices.length + 1; n++){
+            const v = face.vertices[n % face.vertices.length];
+            const vertex = copy.vertices[v];
             ctx.lineTo(offset_x + vertex[0] * scale, offset_y + vertex[1] * scale);
+        }
+
+        // ctx.fillStyle = colors[colorIndex++];
+        // ctx.fill();
+        ctx.strokeStyle = colors[colorIndex++];
         ctx.stroke();
     }
+
+    window.copy = copy;
 }
 
-drawModel(cuboModel, 100, 800/2 - 100/2, 600/2 - 100/2);
+renderModel(cuboModel, 100, 800/2 - 100/2, 600/2 - 100/2);
