1636eae2c677998ba63532f363cf3870974aee54
Patrick Pissurno <contato@patrickpissurno.com.br>
2019-09-04 11:17:18 -0300

    wip

--- main.js
+++ main.js
@@ -1,3 +1,58 @@
+class Vertex {
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} z 
+     */
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Face {
+    /** @param {Vertex[]} vertices */
+    constructor(vertices) {
+        this.vertices = vertices;
+    }
+}
+
+class Model {
+    /** @param {Face[]} faces */
+    constructor(faces){
+        this.faces = faces;
+    }
+}
+
+class Object {
+    /** @param {Model} model */
+    constructor(model){
+        this.model = model;
+    }
+}
+
+const cuboModel = new Model([
+    new Face([ //face traseira
+        new Vertex(0,0,0),
+        new Vertex(1,0,0),
+        new Vertex(1,1,0),
+        new Vertex(0,1,0),
+    ]),
+    new Face([ //face frontal
+        new Vertex(0,0,1),
+        new Vertex(1,0,1),
+        new Vertex(1,1,1),
+        new Vertex(0,1,1),
+    ]),
+    new Face([
+        new Vertex(0,0,0),
+        new Vertex(1,0,0),
+        new Vertex(1,1,0),
+        new Vertex(0,1,0),
+    ]),
+])
+
 const canvas = document.getElementById("canvas");
 
 /** @type {CanvasRenderingContext2D} */
